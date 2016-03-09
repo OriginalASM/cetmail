9d8406c2660232189ce4a3fef4932221b649d1b6
shubham prasad <spd2192@gmail.com>
2016-03-09 19:09:26 +0530

    init angular

---
+++ public/app/scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name App
+ * @description
+ * # App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('App', [
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
