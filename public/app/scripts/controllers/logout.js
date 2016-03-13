4d676dd1bbe269992dbc7aded8d5f0f015d60728
Rishav Agarwal <rishav.159@gmail.com>
2016-03-14 00:13:40 +0530

    Added logout route

--- public/app/scripts/controllers/logout.js
+++ public/app/scripts/controllers/logout.js
@@ -8,10 +8,18 @@
  * Controller of the App
  */
 angular.module('App')
-  .controller('LogoutCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('LogoutCtrl', function ($scope,$http) {
+    $scope.message='Please Wait';
+    $http({
+      method : 'POST',
+      url : '/users/api/logout'
+    }).success(function(data){
+      if(data.message='success'){
+        $scope.message='You have successfully logged out !';
+      }else{
+        $scope.message='There was some problem logging out !';
+      }
+    }).error(function(err){
+      $scope.message='Cannot connect to the server !';
+    });
   });
