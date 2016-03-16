08f559159fe5a524b4e30c34914494fcefd333ee
Rishav Agarwal <rishav.159@gmail.com>
2016-03-16 23:51:54 +0530

    Register page submits data successfully

--- public/app/scripts/controllers/register.js
+++ public/app/scripts/controllers/register.js
@@ -8,9 +8,38 @@
  * Controller of the App
  */
 angular.module('App')
-  .controller('RegisterCtrl', function ($scope) {
+  .controller('RegisterCtrl', function ($scope,$location,$http,$rootScope,$mdDialog) {
     $scope.Credentials = {};
     $scope.data = {
       gender : 'Male'
-    }
+    };
+    $scope.Register = function($event){
+      var Credentials = angular.copy($scope.Credentials);
+      Credentials.gender = $scope.data.gender.charAt(0).toLowerCase() + $scope.data.gender.slice(1);;
+      console.log(Credentials);
+      $http({
+        method : 'POST',
+        url : '/users/api/insert',
+        data : Credentials
+      }).success(function(data){
+        if(data.error){
+          console.log('The data cannot be inserted.');
+        }else{
+          console.log('Registration Successful !');
+
+            $mdDialog.show(
+              $mdDialog.alert()
+                .parent(angular.element(document.querySelector('#logindiv')))
+                .clickOutsideToClose(true)
+                .title('Registration Succesful.')
+                .textContent('Your account Registration is Successful. Please login to continue.')
+                .ok('Got it!')
+            );
+
+            $location.path('/inbox');
+        };
+      }).error(function(err){
+        console.log('There has been the following error : '+err);
+      });
+    };
   });
