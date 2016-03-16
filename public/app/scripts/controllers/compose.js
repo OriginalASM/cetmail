88adc7ca920da3e39b31b8ae4d4ae77489790940
shubham prasad <spd2192@gmail.com>
2016-03-16 21:36:29 +0530

    send mail working

---
+++ public/app/scripts/controllers/compose.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name App.controller:ComposeCtrl
+ * @description
+ * # ComposeCtrl
+ * Controller of the App
+ */
+angular.module('App')
+  .controller('ComposeCtrl', function ($scope, $http) {
+    $scope.mail = {
+      password : 'shubham'
+    };
+    $scope.sendMail = function(){
+      $http({
+        method:'post',
+        url: '/mail/new/simple/',
+        headers: {'Content-Type': 'application/json'},
+        data: $scope.mail
+      }).success(function(s){
+        console.log(s);
+        alert(s);
+      }).error(function(e){
+        console.error(e);
+      });
+    }
+  });
