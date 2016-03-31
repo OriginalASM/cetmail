ffe940da7f5e79f159eb63698a6fc8914d692648
shubham prasad <spd2192@gmail.com>
2016-03-31 18:30:17 +0530

    added temporary solution to get password

--- public/app/scripts/controllers/compose.js
+++ public/app/scripts/controllers/compose.js
@@ -8,9 +8,9 @@
  * Controller of the App
  */
 angular.module('App')
-  .controller('ComposeCtrl', function ($scope, $http,$rootScope,$mdDialog) {
+  .controller('ComposeCtrl', function ($scope, $http,$rootScope,$mdDialog, MailboxPassword) {
     $scope.mail = {
-      password : $rootScope.pass
+      password : MailboxPassword()
     };
     $scope.sendMail = function(){
       $http({
@@ -21,66 +21,56 @@ angular.module('App')
       }).success(function(s){
         console.log(s);
         alert(s);
-
       }).error(function(e){
         console.error(e);
       });
-}
+    };
 
+    $scope.showAlert = showAlert;
+    $scope.showDialog = showDialog;
 
-var alert;
-$scope.showAlert = showAlert;
-$scope.showDialog = showDialog;
-$scope.items = [1, 2, 3];
+    function showAlert(reason) {
+      var alert = $mdDialog.alert({
+        title: 'Attention',
+        textContent: reason,
+        ok: 'Close'
+      });
 
-function showAlert() {
-  alert = $mdDialog.alert({
-    title: 'Attention',
-    textContent: 'This is an example of how easy dialogs can be!',
-    ok: 'Close'
-  });
-  $mdDialog
-    .show(alert)
-    .finally(function() {
-      alert = undefined;
-    });
-}
-
-
-function showDialog($event) {
-
-
-  $scope.disableCompose = true;
-  var parentEl = angular.element(document.body);
-  $mdDialog.show({
-    parent: parentEl,
-    targetEvent: $event,
-    templateUrl: 'views/compose.html',
-    locals: {
-      items: $scope.items
-    },
-    controller: DialogController
-  });
-
-
-
-
-
-
-  function DialogController($scope, $mdDialog, items) {
-    $scope.items = items;
-    $scope.closeDialog = function() {
-      $scope.disableCompose = false;
-      $mdDialog.hide();
-      active();
+      $mdDialog
+        .show(alert)
+        .finally(function() {
+          alert = undefined;
+        });
     }
-  }
-}
 
-function active() {
-  $scope.disableCompose = false;
 
-}
+    function showDialog($event) {
+      $scope.disableCompose = true;
+      var parentEl = angular.element(document.body);
+      $mdDialog.show({
+        parent: parentEl,
+        targetEvent: $event,
+        templateUrl: 'views/compose.html',
+        locals: {
+          items: $scope.items
+        },
+        controller: DialogController
+      });
+
+      function DialogController($scope, $mdDialog, items) {
+        $scope.items = items;
+        $scope.closeDialog = function() {
+          $scope.disableCompose = false;
+          $mdDialog.hide();
+          active();
+        }
+      }
+    }
+
+    function active() {
+      $scope.disableCompose = false;
+
+    }
 
 
 
---
+++ public/app/scripts/services/mailboxpassword.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name App.MailboxPassword
+ * @description
+ * # MailboxPassword
+ * Service in the App.
+ */
+angular.module('App')
+  .service('MailboxPassword', function ($rootScope) {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+    return function(){
+      if(!$rootScope.pass){
+        var result = window.prompt("Provide Inbox Password");
+        $rootScope.pass = result ;
+        return result ;
+      }else
+        return $rootScope.pass ;
+      }
+  });
